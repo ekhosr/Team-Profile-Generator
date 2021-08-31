43ece416032be403c36bff8d23b62ef6db8b3cf9
Ehsan Khosravi <ekhosr@gmail.com>
2021-08-31 13:54:23 -0400

    engineer test added

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,24 +1,23 @@
 const Employee = require("../lib/Employee");
 
 test("create employee", () => {
-  const employee = new Employee("Ehsan");
+  const employee = new Employee("EmployeeName");
 });
 
 test("Set id", () => {
   const testValue = 100;
-  const e = new Employee("Ehsan", testValue);
+  const e = new Employee("Emp", testValue);
   expect(e.id).toBe(testValue);
 });
 
-test("Set Email ", () => {
-  const testValue = "ehsan@email.com";
-  const e = new Employee("Ehsan", 1, testValue);
+test("Set Email", () => {
+  const testValue = "employee@email.com";
+  const e = new Employee("Emp", 1, testValue);
   expect(e.email).toBe(testValue);
 });
 
-// Test if the getRole() value is Employee
 test("getRole() return Employee", () => {
   const testValue = "Employee";
-  const e = new Employee("Ehsan", 1, "ehsan@email.com");
+  const e = new Employee("EmployeeName", 1, "employee@email.com");
   expect(e.getRole()).toBe(testValue);
 });
---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,25 @@
+const Employee = require("../lib/Employee");
+const Engineer = require("../lib/Engineer");
+
+test("Create Engineer", () => {
+  const engineer = new Engineer("EngineerName");
+});
+
+test("Set Github Account", () => {
+  const testValue = "GitHubAccount";
+  const e = new Engineer("Eng", 1, "engineer@email.com", testValue);
+  expect(e.github).toBe(testValue);
+});
+
+test("Get Github Account ", () => {
+  const testValue = "GitHubAccount";
+  const e = new Engineer("Eng", 1, "engineer@email.com", testValue);
+  expect(e.getGitHub()).toBe(testValue);
+});
+
+// Test if the getRole() value is Engineer
+test("getRole() return Engineer", () => {
+  const testValue = "Engineer";
+  const e = new Engineer("Eng", 1, "engineer@email.com", "GitHubAccount");
+  expect(e.getRole()).toBe(testValue);
+});
